db6a842a01ad2f5039eb82570b05f89a9b2a51d4
Tremaine Buchanan <tremainekbuchanan@gmail.com>
2016-11-12 18:49:35 -0500

    Changed js file

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,8 @@
-function myFunction() {
-    alert("This state does not offer online verification");}
+/**
+ * Alerts user if the state doesn't offer
+ * online verification.
+ */
+function alertUser(){
+	alert("This state does not offer online verification")
+}
+
